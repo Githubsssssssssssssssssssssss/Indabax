fca2dd36bf20006e4b4000306f82b472d8f10742
steeve <rodriguetakougoum@gmail.com>
2025-04-07 11:15:50 +0100

    Update

--- es.py
+++ es.py
@@ -47,7 +47,8 @@ def hash_password(password):
 # Fonction pour vérifier les identifiants
 def check_credentials(username, password):
     users = {
-        word()[0]: hash_password(word()[1]),
+        "Admin": hash_password("Admin"),
+        #word()[0]: hash_password(word()[1]),
     }
     if username in users and users[username] == hash_password(password):
         return True
